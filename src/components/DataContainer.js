63ef0504ef49150e48ece30b4d6f25223ebbdbee
Phil Arfuso <Arfusop@gmail.com>
2019-02-27 21:37:36 -0500

    Added in ability to change payouts per quarter, and total is now calculating the payout per quarter.

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -1,95 +1,111 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Card from "./styles/Card";
 import CardHeader from "./styles/CardHeader";
 import CardBody from "./styles/CardBody";
 
 export default class DataContainer extends Component {
-  state = {
-    box: null,
-    initials: null
-  };
+	state = {
+		box: null,
+		initials: null,
+		showModal: false
+	};
 
-  handleChange = (e, label) => {
-    this.setState({
-      [label]: label === "box"
-        ? parseInt(e.target.value)
-        : e.target.value
-    });
-  };
+	handleChange = (e, label) => {
+		// allow for ints ONLY
+		// allow for comma separated ints
+		this.setState({
+			[label]: label === "box" ? parseInt(e.target.value) : e.target.value
+		});
+	};
 
-  render() {
-    const {allowReset, assignScoreNumbers, filledBoxes, handleDataInput, resetNumbers} = this.props;
-    const {box, initials} = this.state;
+	handleEmailClick = () => {
+		console.log("show modal");
+		// 1. take a canvas screen shot of the payouts & the box grid
+		// 2. open modal
+		// 3. append the canvas images to the modal
+		// 4. modal needs a confirm send email btn
+	};
 
-    return (
-      <Card
-        gtr="40px 1fr"
-        border="1px solid #EEF0F2"
-        boxShadow="0 8px 6px -6px #FFE053">
-        <CardHeader color="#fff" bgColor="#6d757d">
-          Assign Boxes:
-        </CardHeader>
-        {filledBoxes < 100
-          ? (
-            <CardBody bgColor="#011638" color="#fff" gridgap="10px">
-              <div
-                style={{
-                display: "grid",
-                gridTemplateColumns: "1fr 1fr",
-                gridGap: "10px"
-              }}>
-                <input
-                  id="boxNumbers"
-                  className="inputs boxesInput"
-                  placeholder="Box #s"
-                  type="number"
-                  min="1"
-                  max="100"
-                  defaultValue={box}
-                  onChange={e => this.handleChange(e, "box")}/>
-                <input
-                  id="initials"
-                  className="inputs initialsInput"
-                  placeholder="Initials"
-                  type="text"
-                  defaultValue={initials}
-                  onChange={e => this.handleChange(e, "initials")}/>
-              </div>
-              <button
-                disabled={box == null || initials == null}
-                className="submitBtn"
-                onClick={() => handleDataInput({box, initials})}>
-                Submit&nbsp;&nbsp;{" "} {box == null || initials == null
-                  ? (<i className="fas fa-ban"/>)
-                  : (<i className="fas fa-check-circle"/>)}
-              </button>
-            </CardBody>
-          )
-          : (
-            <CardBody bgColor="#001539" gtr="1fr">
-              {allowReset
-                ? (
-                  <div className="shareContainer">
-                    <button onClick={resetNumbers} className="shareBtns">
-                      <i className="fas fa-trash-alt"></i>
-                    </button>
-                    <button className="shareBtns">
-                      <i className="fas fa-envelope"></i>
-                    </button>
-                    <button onClick={() => window.print()} className="shareBtns">
-                      <i className="fas fa-print"></i>
-                    </button>
-                  </div>
-                )
-                : (
-                  <button className="numberAssign" onClick={assignScoreNumbers}>
-                    Assign Scores
-                  </button>
-                )}
+	render() {
+		const {
+			allowReset,
+			assignScoreNumbers,
+			filledBoxes,
+			handleDataInput,
+			resetNumbers
+		} = this.props;
+		const { box, initials } = this.state;
 
-            </CardBody>
-          )}
-      </Card>
-    );
-  }
+		return (
+			<Card
+				gtr="40px 1fr"
+				border="1px solid #EEF0F2"
+				boxShadow="0 8px 6px -6px #FFE053"
+			>
+				<CardHeader color="#fff" bgColor="#6d757d">
+					Assign Boxes:
+				</CardHeader>
+				{filledBoxes < 100 ? (
+					<CardBody bgColor="#011638" color="#fff" gridgap="10px">
+						<div
+							style={{
+								display: "grid",
+								gridTemplateColumns: "1fr 1fr",
+								gridGap: "10px"
+							}}
+						>
+							<input
+								id="boxNumbers"
+								className="inputs boxesInput"
+								placeholder="Box #s"
+								type="string"
+								defaultValue={box}
+								onChange={e => this.handleChange(e, "box")}
+							/>
+							<input
+								id="initials"
+								className="inputs initialsInput"
+								placeholder="Initials"
+								type="text"
+								defaultValue={initials}
+								onChange={e => this.handleChange(e, "initials")}
+							/>
+						</div>
+						<button
+							disabled={box == null || initials == null}
+							className="submitBtn"
+							onClick={() => handleDataInput({ box, initials })}
+						>
+							Submit&nbsp;&nbsp;{" "}
+							{box == null || initials == null ? (
+								<i className="fas fa-ban" />
+							) : (
+								<i className="fas fa-check-circle" />
+							)}
+						</button>
+					</CardBody>
+				) : (
+					<CardBody bgColor="#001539" gtr="1fr">
+						{allowReset ? (
+							<div className="shareContainer">
+								<button onClick={resetNumbers} className="shareBtns">
+									<i className="fas fa-trash-alt" />
+								</button>
+								<button className="shareBtns">
+									<i className="fas fa-envelope" />
+								</button>
+								<button onClick={() => window.print()} className="shareBtns">
+									<i className="fas fa-print" />
+								</button>
+							</div>
+						) : (
+							<button className="numberAssign" onClick={assignScoreNumbers}>
+								Assign Scores
+							</button>
+						)}
+					</CardBody>
+				)}
+			</Card>
+		);
+	}
 }
--- src/components/Payout.js
+++ src/components/Payout.js
@@ -1,38 +1,92 @@
-import React, {Component} from 'react'
-import Card from '../components/styles/Card'
-import CardHeader from '../components/styles/CardHeader'
-import CardBody from '../components/styles/CardBody'
+import React, { Component } from "react";
+import Card from "../components/styles/Card";
+import CardHeader from "../components/styles/CardHeader";
+import CardBody from "../components/styles/CardBody";
 
 export default class Payout extends Component {
-  state = {
-    q1: 500,
-    q2: 1000,
-    q3: 500,
-    q4: 2500,
-    total: 5000
-  };
+	state = {
+		q1: 0,
+		q2: 0,
+		q3: 0,
+		q4: 0,
+		total: 0
+	};
 
-  render() {
-    const {q1, q2, q3, q4, total} = this.state;
-    return (
-      <Card
-        gtr="40px 1fr"
-        border="1px solid #EEF0F2"
-        boxShadow="0 8px 6px -6px #FFE053">
-        <CardHeader color="#fff" bgColor="#6d757d">Payouts</CardHeader>
-        <CardBody bgColor="#011638" color="#fff">
-          <div>1st Quarter: ${q1}</div>
-          <div>2nd Quarter: ${q2}</div>
-          <div>3rd Quarter: ${q3}</div>
-          <div>4th Quarter: ${q4}</div>
-          <div
-            style={{
-            width: '100%',
-            borderTop: '2px solid #EEF0F2'
-          }}/>
-          <div>Total: ${total}</div>
-        </CardBody>
-      </Card>
-    )
-  }
+	handleChange = e => {
+		const { value, name } = e.target;
+		this.setState(
+			{
+				[name]: value
+			},
+			() => {
+				const { q1, q2, q3, q4 } = this.state;
+				const newTotal = Number(q1) + Number(q2) + Number(q3) + Number(q4);
+				this.setState({ total: newTotal });
+			}
+		);
+	};
+
+	render() {
+		const { q1, q2, q3, q4, total } = this.state;
+		return (
+			<Card
+				gtr="40px 1fr"
+				border="1px solid #EEF0F2"
+				boxShadow="0 8px 6px -6px #FFE053"
+			>
+				<CardHeader color="#fff" bgColor="#6d757d">
+					Payouts
+				</CardHeader>
+				<CardBody bgColor="#011638" color="#fff">
+					<div className="payoutContainers">
+						<span>Q1: $</span>
+						<input
+							name="q1"
+							className="payoutInput"
+							type="text"
+							onChange={e => this.handleChange(e)}
+							defaultValue={q1}
+						/>
+					</div>
+					<div className="payoutContainers">
+						<span>Q2: $</span>
+						<input
+							name="q2"
+							className="payoutInput"
+							type="text"
+							onChange={e => this.handleChange(e)}
+							defaultValue={q2}
+						/>
+					</div>
+					<div className="payoutContainers">
+						<span>Q3: $</span>
+						<input
+							name="q3"
+							className="payoutInput"
+							type="text"
+							onChange={e => this.handleChange(e)}
+							defaultValue={q3}
+						/>
+					</div>
+					<div className="payoutContainers">
+						<span>Q4: $</span>
+						<input
+							name="q4"
+							className="payoutInput"
+							type="text"
+							onChange={e => this.handleChange(e)}
+							defaultValue={q4}
+						/>
+					</div>
+					<div
+						style={{
+							width: "100%",
+							borderTop: "2px solid #EEF0F2"
+						}}
+					/>
+					<div>Total: ${total}</div>
+				</CardBody>
+			</Card>
+		);
+	}
 }
